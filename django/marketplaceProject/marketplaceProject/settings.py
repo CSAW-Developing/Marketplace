c12ec812c4eff43ef73b10a0ea69a5d06e6f0a77
Adam Wong <adam.wong789@googlemail.com>
2024-08-22 03:18:11 +0100

    Added requirements, linked front and backend using django cors headers

--- django/marketplaceProject/marketplaceProject/settings.py
+++ django/marketplaceProject/marketplaceProject/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+
 
 ROOT_URLCONF = 'marketplaceProject.urls'
 
